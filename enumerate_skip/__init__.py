6dfe12547886afe27b177853b3d7404fadf0cdd8
Wieland Hoffmann <themineo@gmail.com>
2017-02-25 19:31:03 +0100

    Use setuptools_scm

--- enumerate_skip/__init__.py
+++ enumerate_skip/__init__.py
@@ -1,5 +1,6 @@
-# Copyright (c) 2014 Wieland Hoffmann
+# Copyright (c) 2014, 2017 Wieland Hoffmann
 # License: MIT, see LICENSE for details
 from .funcs import enumerate_manual, enumerate_skip
+from .version import version as __version__ # noqa
 
-__all__ = ["enumerate_manual", "enumerate_skip"]
+__all__ = ["enumerate_manual", "enumerate_skip", "__version__"]
